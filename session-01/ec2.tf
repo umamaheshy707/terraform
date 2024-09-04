fcaae0867dd2781e10a48c94fa5f62fafb490457
umamaheshy707 <ummamaheswararao446@gmail.com>
2024-09-04 18:06:29 +0530

    added

---
+++ session-01/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "example_server" {
+  ami           = "ami-010a58cb39049ee7b"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
+  tags = {
+    Name = "devops"
+  }
+}
+resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
+    name        = var.sg-name # this is for AWS
+    description = var.sg-description
+    #vpc_id      = aws_vpc.main.id
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = var.inbound-from-port # 0 means all ports
+        to_port          = 0 
+        protocol         = "tcp"
+        cidr_blocks      = var.cidr_blocks
+        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "roboshop-all-aws"
+    }
+}
